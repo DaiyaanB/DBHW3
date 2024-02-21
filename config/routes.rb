00be209fe92ca8d89affa53f65e013eb00b22751
Daiyaan B. <daiyaan.butt@kellogg.northwestern.edu>
2024-02-21 23:07:37 +0000

    Generated places controller and indices

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  get 'places/index'
+  get 'places/show'
+  get 'places/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :places #Route for places controller
   root 'places#index'
-  
-  # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
 end
