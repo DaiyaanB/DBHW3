67c5a9a6ed910105db8fe294b7c5e01b6b203bdb
Daiyaan B. <daiyaan.butt@kellogg.northwestern.edu>
2024-02-21 23:12:07 +0000

    Controller for places and view files

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,10 +1,29 @@
-class PlacesController < ApplicationController
+class PlacesController < ApplicationController #Built with the help of GPT 4.0 based on class lecture notes and HW goals for user stories
+  
   def index
+    @places = Place.all
   end
-
+  
   def show
+    @place = Place.find(params[:id])
   end
-
+  
   def new
+    @place = Place.new
   end
+  
+  def create
+    @place = Place.new(place_params)
+    if @place.save
+      redirect_to places_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def place_params
+    params.require(:place).permit(:name)
+  end
+  
 end
