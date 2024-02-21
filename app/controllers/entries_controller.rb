b838f45eaf7ec404e873f580c9d4958331919754
Daiyaan B. <daiyaan.butt@kellogg.northwestern.edu>
2024-02-21 23:36:59 +0000

    Part II

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,23 @@
-class EntriesController < ApplicationController
+class EntriesController < ApplicationController #Made with GPT, Class notes, and requirements from HW 3
   def new
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.build
   end
+  
 
   def create
+    @place = Place.find(params[:place_id])
+    @entry = @place.entries.build(entry_params)
+    if @entry.save
+      redirect_to place_path(@place)
+    else
+      render :new
+    end
   end
+  
+  private
+  
+  def entry_params
+    params.require(:entry).permit(:title, :description, :posted_on)
+  end  
 end
