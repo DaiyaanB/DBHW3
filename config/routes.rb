a322cb0db100c36de6bf955224859ffe9886b635
Daiyaan B. <daiyaan.butt@kellogg.northwestern.edu>
2024-02-21 23:06:58 +0000

    First edits to config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :places #Route for places controller
+  root 'places#index'
+  
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
